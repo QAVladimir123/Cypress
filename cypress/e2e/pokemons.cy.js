4c1d37b21bad5174d2a65b90b6e1f76f2bece0de
Vladimir <vovabelous@mail.ru>
2024-11-06 19:59:10 +0300

    Delete...

--- cypress/e2e/pokemons.cy.js
+++ cypress/e2e/pokemons.cy.js
@@ -1,20 +1,20 @@
-describe('Покупка аватара', function () {                                // название набора тестов
-    it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
-         cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
-         cy.get('input[type="email"]').type('vovabelous@mail.ru');                   // вводим логин
-         cy.get('input[type="password"]').type('Vladimir1994');               // вводим пароль
-         cy.get('button[type="submit"]').click();                        // нажимаем кнопку Подтвердить
+describe('Покупка аватара', function () {                                
+    it('e2e тест на покупку нового аватара для тренера', function () {   
+         cy.visit('https://pokemonbattle.ru/');                          
+         cy.get('input[type="email"]').type('vovabelous@mail.ru');                  
+         cy.get('input[type="password"]').type('Vladimir1994');              
+         cy.get('button[type="submit"]').click();                       
          cy.wait(2000);
-         cy.get('.header__container > .header__id').click({ force: true }); // Клик в шапке на аву тренера
-         cy.get('[href="/shop"]').click();                               // нажимаем кнопку Магазин
-         cy.get('.available > button').first().click({ force: true });   // кликаем Купить у первого доступного аватара
-         cy.get('.credit').type('4620869113632996');                     // вводим номер карты
-         cy.get('.k_input_ccv').type('125');                             // вводим CVV карты
-         cy.get('.k_input_date').type('1225');                           // вводим срок действия карты
-         cy.get('.k_input_name').type('Vova belous');                           // вводим имя владельца действия карты
-         cy.get('.pay-btn').click();                                     // нажимаем кнопку Оплатить
-         cy.get('#cardnumber').type('56456');                            // вводим код подтверждения СМС
-         cy.get('.payment__submit-button').click();                      // нажимаем кнопку Отправить
-         cy.contains('Покупка прошла успешно').should('be.visible');     // проверяем наличие и видимость сообщения о успешной покупке
+         cy.get('.header__container > .header__id').click({ force: true }); 
+         cy.get('[href="/shop"]').click();                               
+         cy.get('.available > button').first().click({ force: true });   
+         cy.get('.credit').type('4620869113632996');                     
+         cy.get('.k_input_ccv').type('125');                             
+         cy.get('.k_input_date').type('1225');                           
+         cy.get('.k_input_name').type('Vova belous');                          
+         cy.get('.pay-btn').click();                                    
+         cy.get('#cardnumber').type('56456');                            
+         cy.get('.payment__submit-button').click();                    
+         cy.contains('Покупка прошла успешно').should('be.visible');    
      });
  });
